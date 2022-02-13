67a95c4a07514da126eae7a6b1d53c761beff447
Radius17 <radius17@gmail.com>
2022-02-13 17:48:44 +0300

    Баг фикс скрипта получения id пользователей Telegram

--- recipients-setup.py
+++ recipients-setup.py
@@ -1,5 +1,10 @@
 import sys, json
 
 for update in json.load(sys.stdin)['result']:
+#  print('-----------------------------')
+#  print(update)
+  if 'message' in update:
     from_=update['message']['from']
     print(from_.get('id','<undefined>'),from_.get('first_name','<undefined>'),from_.get('last_name','<undefined>'), from_.get('username','<undefined>'))
+#  else:
+#    print(":(")
